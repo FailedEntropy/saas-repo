9d7142f790aeeb8b446e5e1fc4fee0b86a25be0e
FailedEntropy <saasbook@saasbook.(none)>
2012-08-09 13:03:28 -0700

    Impemented filtering. But it fails when after sorting + sorting fails after filtering

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,7 @@
 class MoviesController < ApplicationController  
+  before_filter :collect_ratings
   attr_accessor :hilite_headers 
+  
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
@@ -8,12 +10,16 @@ class MoviesController < ApplicationController
 
   def index
     @hilite_headers = {"title" => :common, "release_date" => :common}
+    params.has_key?(:ratings) ? rates = params[:ratings].keys : rates = "*"
+    if rates.class == Array then rates.each {|x| @checked_checkboxes[x] = true} end
+
     if params.has_key?(:sort_by)
       header = params[:sort_by]
       @hilite_headers[header] = :hilite
-      @movies = Movie.find(:all, :order => header)
+      session[:sorted_by] = header
+      @movies = Movie.find(:all, :order => header, :conditions => {:rating => rates})
     else
-      @movies = Movie.all
+      @movies = Movie.find(:all, :conditions => {:rating => rates})
     end
   end
 
@@ -43,5 +49,12 @@ class MoviesController < ApplicationController
     @movie.destroy
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
-  end 
+  end
+    
+  private
+    def collect_ratings
+      @all_ratings = Movie.select(:rating).map(&:rating).uniq
+      @checked_checkboxes = Hash.new
+      @all_ratings.each {|x| @checked_checkboxes[x] = false}
+    end  
 end
